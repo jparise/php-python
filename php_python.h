3ca9ebe92f24bcf0cb2e90b2e3374bc1bd9d579d
Jon Parise <jon@php.net>
2002-07-29 07:51:39 +0000

    Add my embedded Python extension to PECL.  This does not represent a full release.

---
+++ php_python.h
@@ -0,0 +1,49 @@
+/*
+   +----------------------------------------------------------------------+
+   | PHP Version 4                                                        |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 1997-2002 The PHP Group                                |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 2.02 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available at through the world-wide-web at                           |
+   | http://www.php.net/license/2_02.txt.                                 |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Author: Jon Parise <jon@php.net>                                     |
+   +----------------------------------------------------------------------+
+ */
+
+ /* $Id$ */
+
+#ifndef PHP_PYTHON_H
+#define PHP_PYTHON_H
+
+extern zend_module_entry python_module_entry;
+#define phpext_python_ptr &python_module_entry
+
+#ifdef PHP_WIN32
+#define PHP_PYTHON_API __declspec(dllexport)
+#else
+#define PHP_PYTHON_API
+#endif
+
+#ifdef ZTS
+#include "TSRM.h"
+#endif
+
+#ifdef ZTS
+#define W32_G(v) TSRMG(python_globals_id, zend_python_globals *, v)
+#else
+#define W32_G(v) (python_globals.v)
+#endif
+
+PHP_MINFO_FUNCTION(python);
+PHP_MINIT_FUNCTION(python);
+PHP_MSHUTDOWN_FUNCTION(python);
+
+PHP_FUNCTION(py_eval);
+
+#endif /* PHP_PYTHON_H */
